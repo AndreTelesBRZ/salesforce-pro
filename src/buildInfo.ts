fcfda8f2aa7bf28424b313a20113534ebc266792
Andre Teles <andre.araujo.teles@gmail.com>
2026-06-27 10:39:02 -0300

    Release 1.0.5 build 20260627.002

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
   version: "1.0.5",
-  build: "20260627.001",
-  commit: "d1207ca",
-  buildDate: "2026-06-27T13:25:51.000Z",
+  build: "20260627.002",
+  commit: "bf91732",
+  buildDate: "2026-06-27T13:38:40.419Z",
 } as const;
