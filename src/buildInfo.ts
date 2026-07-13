1d57060546a33d93039dcab1fb4e746c54c089fb
Andre Teles <andre.araujo.teles@gmail.com>
2026-07-13 07:04:12 -0300

    Ajustes rotas

--- src/buildInfo.ts
+++ src/buildInfo.ts
@@ -1,6 +1,6 @@
 export const BUILD_INFO = {
   version: "1.0.5",
   build: "20260713.001",
-  commit: "76d2896",
-  buildDate: "2026-07-13T09:25:42.888Z",
+  commit: "cbf0201",
+  buildDate: "2026-07-13T10:02:52.632Z",
 } as const;
